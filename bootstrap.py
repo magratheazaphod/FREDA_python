f94d4ea31c293fe48333b28f78c7faea97fab906
magratheazaphod <jessed@berkeley.edu>
2016-09-09 11:01:03 -0700

    continuing to add bootstrap methods

--- bootstrap.py
+++ bootstrap.py
@@ -4,34 +4,77 @@
 #elementary bootstrap - given data, returns standard deviation of mean by bootstrapping (as well as mean)
 import numpy as np
 import numpy.random as rnd
+import time
 
-#V is the sample space, nsamp is the number of samples that we're drawing
+
+#Bootstrap resampling of sample data
+#V is the sample space, nsamp is the number of samples that we're drawing from V
 def bs_resample(V,nsamp):
+    
     nn = len(V)
     indices = np.floor(nn * rnd.random_sample((nsamp,))).astype(int)
     Vnew = np.array(V[indices])
     return Vnew
+
+#a resampling method that takes into account temporal autocorrelation by drawing data in consecutive blocks:
+#the "Moving Blocks Bootstrap" - Künsch (1988)
+def bs_resample_block(V,nsamp,blklen):
     
+    nn = len(V)
+    nblks = np.ceil(nsamp/blklen)
+    
+    #the number of possible different blocks is nn-blklen+1
+    indices = np.floor((nn-blklen+1) * rnd.random_sample((nblks,))).astype(int)    
+    Vnew = np.zeros(nsamp)
+    
+    for i in np.arange(nblks):
+        
+        #final block may not be of full length - must account for it
+        myblklen = np.minimum(blklen,nsamp-blklen*i)
+        Vnew[blklen*i : (blklen*i+myblklen)] = V[indices[i] : indices[i]+myblklen]
+
+    return Vnew
+
 #using the preceding bs_resample function, finds std dev of mean by bootstrap redrawing
 #returns actual mean and its standard deviation based on bootstrapping
-def bs_mean(V,niter, axis=2):
-    nn = len(V)
-    means = np.empty((niter,))
+def bs_stdofmean(V, niter):
     
-    for i in np.range(niter):
-        means[i] = np.mean(bs_resample(V,nn)))
+    nn = len(V)
+    means = np.zeros((niter,))
+    
+    for i in np.arange(niter):
+        means[i] = np.mean(bs_resample(V,nn))
         
     means_std = np.std(means)
     return np.mean(V), means_std
 
-def bs_mean_blocks(V,blklen,niter):
+#equivalent to bs_stdofmean, but again, with samples drawn in consecutive blocks of length blklen
+def bs_stdofmean_block(V, niter, blklen):
     
-    return
+    nn = len(V)
+    means = np.zeros((niter,))
+    
+    for i in np.arange(niter):
+        means[i] = np.mean(bs_resample_block(V, nn, blklen))
+        
+    means_std = np.std(means)
+    return np.mean(V), means_std
 
-def bs_diff(V1,V2,blklen,niter):
 
-    return
+#code assesses the statistical significance of the difference in means between two data sets.
+#two different (largely equivalent) techniques are implemented: bootstrapping with replacement and a permutation method (bootstrapping without replacement)
+#Default: Bootstrap with replacement, can be specified as keyword.
+
+def bs_means_diff(V1,V2,niter,method='bootstrap'):
+
+    n1 = len(V1)
+    n2 = len(V2)
+    V = np.append(V1,V2)
+    nn = len(V)
+    
+    return V
     
 def bs_diff_blocks(V1,V2,blklen,niter):
 
+    
     return
